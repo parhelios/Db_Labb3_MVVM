6b1d93d36a0cd154f89532e040106c083bf8c189
Tobias Lindberg <uet.lindberg@outlook.com>
2024-01-18 17:24:50 +0100

    -

--- Db_Labb3_MVVM/App.xaml.cs
+++ Db_Labb3_MVVM/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Db_Labb3_MVVM.Managers.Interfaces;
+using Db_Labb3_MVVM.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Db_Labb3_MVVM
@@ -16,15 +19,23 @@ namespace Db_Labb3_MVVM
         {
             //Sätta upp alla beroenden och instansieringar
             AppHost = Host.CreateDefaultBuilder()
-                
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<IDataManager, IDataManager>();
+
+                    services.AddSingleton<MainWindow>();
+                })
                 .Build();
         }
 
         protected override async void OnStartup(StartupEventArgs e)
         {   
-            AppHost.StartAsync();
+            AppHost.StartAsync(); 
 
-            //Sätta upp MainWindow
+            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            mainWindow.DataContext = new MainWindowViewModel(AppHost.Services.GetRequiredService<IDataManager>());
+
+            mainWindow.Show();
 
             base.OnStartup(e);
         }
--- Db_Labb3_MVVM/ViewModels/MainWindowViewModel.cs
+++ Db_Labb3_MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Db_Labb3_MVVM.Managers.Interfaces;
 
 namespace Db_Labb3_MVVM.ViewModels;
 
 public class MainWindowViewModel : ObservableObject
 {
-    
+    private readonly IDataManager _dataManager;
+
+    public MainWindowViewModel(IDataManager dataManager)
+    {
+        _dataManager = dataManager;
+    }
 }
