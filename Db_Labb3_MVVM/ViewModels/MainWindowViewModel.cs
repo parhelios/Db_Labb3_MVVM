d3e1a4d27b50a88c582a5d4ad5f1b6d0bf12162d
Tobias Lindberg <uet.lindberg@outlook.com>
2024-01-18 17:33:55 +0100

    slut för idag, tack för idag

--- Db_Labb3_MVVM/ViewModels/MainWindowViewModel.cs
+++ Db_Labb3_MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Db_Labb3_MVVM.Managers.Interfaces;
+using Db_Labb3_MVVM.Models;
+using ZstdSharp.Unsafe;
 
 namespace Db_Labb3_MVVM.ViewModels;
 
@@ -7,8 +9,19 @@ public class MainWindowViewModel : ObservableObject
 {
     private readonly IDataManager _dataManager;
 
+    private readonly DataModel _dataModel;
+
+    private int _counter;
+
+    public int Counter
+    {
+        get => _counter;
+        set => SetProperty(ref _counter, value);
+    }
+
     public MainWindowViewModel(IDataManager dataManager)
     {
         _dataManager = dataManager;
+        _dataModel = _dataManager.DataModel; 
     }
 }
