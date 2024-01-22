3148603c1c9427bd581bd59186b673b026c099e5
Tobias Lindberg <uet.lindberg@outlook.com>
2024-01-22 15:03:16 +0100

    -

--- Db_Labb3_MVVM/ViewModels/CenterViewModel.cs
+++ Db_Labb3_MVVM/ViewModels/CenterViewModel.cs
@@ -1,8 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Db_Labb3_MVVM.Factories.Interfaces;
+using Db_Labb3_MVVM.Managers.Interfaces;
 
 namespace Db_Labb3_MVVM.ViewModels;
 
 public class CenterViewModel : ObservableObject
 {
-    
+    private readonly INavigationManager _navigationManager;
+
+    public CenterViewModel(INavigationManager navigationManager, IViewModelFactory<LeftViewModel> leftFactory)
+    {
+        _navigationManager = navigationManager;
+
+
+    }
 }
