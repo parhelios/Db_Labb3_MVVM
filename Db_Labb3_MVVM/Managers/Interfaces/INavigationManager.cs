d363bcb0ab87fee8c85a609bfe94e7a8ee4fbd8a
Tobias Lindberg <uet.lindberg@outlook.com>
2024-01-22 14:21:48 +0100

    -

---
+++ Db_Labb3_MVVM/Managers/Interfaces/INavigationManager.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Db_Labb3_MVVM.Managers.Interfaces;
+
+public interface INavigationManager
+{
+    ObservableObject CurrentViewModel { get; set; }
+
+    event Action? CurrentViewModelChanged;
+
+    void OnCurrentViewModelChanged();
+}
---
+++ Db_Labb3_MVVM/Managers/NavigationManager.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Db_Labb3_MVVM.Managers.Interfaces;
+
+namespace Db_Labb3_MVVM.Services;
+
+public class NavigationManager : INavigationManager
+{
+    private ObservableObject _currentViewModel;
+
+    public ObservableObject CurrentViewModel
+    {
+        get
+        {
+            return _currentViewModel;
+        }
+        set
+        {
+            _currentViewModel = value;
+            OnCurrentViewModelChanged();
+        }
+    }
+
+    public event Action? CurrentViewModelChanged;
+
+    public void OnCurrentViewModelChanged()
+    {
+        CurrentViewModelChanged?.Invoke();
+    }
+}
