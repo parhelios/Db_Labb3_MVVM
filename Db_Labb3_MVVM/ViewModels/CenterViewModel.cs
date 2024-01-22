30f3453b04ea07639590dfff1e27d6f1363f75bd
Tobias Lindberg <uet.lindberg@outlook.com>
2024-01-22 15:23:45 +0100

    -

--- Db_Labb3_MVVM/ViewModels/CenterViewModel.cs
+++ Db_Labb3_MVVM/ViewModels/CenterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Db_Labb3_MVVM.Factories.Interfaces;
 using Db_Labb3_MVVM.Managers.Interfaces;
 
@@ -8,10 +9,15 @@ public class CenterViewModel : ObservableObject
 {
     private readonly INavigationManager _navigationManager;
 
+    public IRelayCommand NavigateLeftCommand { get; }
+
     public CenterViewModel(INavigationManager navigationManager, IViewModelFactory<LeftViewModel> leftFactory)
     {
         _navigationManager = navigationManager;
 
-
+        NavigateLeftCommand = new RelayCommand(() =>
+        {
+            _navigationManager.CurrentViewModel = leftFactory.Create();
+        });
     }
 }
